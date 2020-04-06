b3f0fbcd0ab3372443e872de0d4b95ac99035eae
jbrown <jbrown@powercosts.com>
2020-04-06 01:59:22 -0500

    Lightly refactored common code to enable code re-use for other complexity types

--- src/main/java/com/brownian/plugins/intellij/complexity/markers/CyclomaticComplexityLineMarker.kt
+++ src/main/java/com/brownian/plugins/intellij/complexity/markers/CyclomaticComplexityLineMarker.kt
@@ -1,56 +1,8 @@
 package com.brownian.plugins.intellij.complexity.markers
 
-import com.intellij.codeInsight.daemon.LineMarkerInfo
-import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
-import com.intellij.psi.*
 import com.siyeh.ig.classmetrics.CyclomaticComplexityVisitor
 
-class CyclomaticComplexityLineMarker : LineMarkerProviderDescriptor() {
-    override fun getName(): String? = "Cyclomatic complexity line marker"
-
-    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-        val parent = element.parent
-        if (element is PsiIdentifier) {
-            if ((parent is PsiMethod && parent.nameIdentifier == element) ||
-                (parent is PsiClass && parent.nameIdentifier == element)
-            ) {
-                return getComplexityMarkerInfo(element, parent)
-            }
-        } else if (element is PsiKeyword) {
-            if (
-                ((element.textMatches(PsiKeyword.SWITCH) || element.textMatches(PsiKeyword.CASE)) && (parent is PsiSwitchStatement || parent is PsiSwitchExpression)) ||
-                (element.textMatches(PsiKeyword.WHILE) && parent is PsiWhileStatement) ||
-                (element.textMatches(PsiKeyword.FOR) && (parent is PsiForStatement || parent is PsiForeachStatement)) ||
-                (element.textMatches(PsiKeyword.DO) && parent is PsiDoWhileStatement) ||
-                (element.textMatches(PsiKeyword.TRY) && parent is PsiTryStatement) ||
-                (element.textMatches(PsiKeyword.CATCH) && parent is PsiCatchSection)
-            ) {
-                return getComplexityMarkerInfo(element, parent)
-            } else if (element.textMatches(PsiKeyword.IF) && parent is PsiIfStatement) {
-                return getComplexityMarkerInfo(
-                    element,
-                    parent.thenBranch ?: return null,
-                    parent.condition ?: return null
-                )
-            } else if (element.textMatches(PsiKeyword.ELSE) && parent is PsiIfStatement && parent.elseBranch !is PsiIfStatement) {
-                return getComplexityMarkerInfo(element, parent.elseBranch ?: return null)
-            }
-        }
-
-        return null
-    }
-
-    private fun <E : PsiElement, P : PsiElement> getComplexityMarkerInfo(
-        elementToMark: E,
-        vararg blocksToMeasure: P
-    ): LineMarkerInfo<*>? {
-        val visitor = CyclomaticComplexityVisitor()
-        blocksToMeasure.forEach { visitor.visitElement(it) }
-        val complexity = visitor.complexity
-        return if (complexity == 1) {
-            null
-        } else {
-            ComplexityLineMarkerInfo(complexity, elementToMark)
-        }
-    }
-}
+class CyclomaticComplexityLineMarker : AbstractComplexityLineMarker<CyclomaticComplexityVisitor>(
+    { CyclomaticComplexityVisitor() },
+    CyclomaticComplexityVisitor::getComplexity
+)
